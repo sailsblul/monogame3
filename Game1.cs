f38e5a86aecbea422e9986ddd859ebd345fd01dd
Silas Bull <BullSila402@GoTVDSB.ca>
2022-05-11 11:04:49 -0400

    monogame6 (but better this time)

--- Game1.cs
+++ Game1.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
 
 namespace monogame3
 {
@@ -11,28 +12,11 @@ namespace monogame3
         float time;
         float timeStamp;
         Random gen = new Random();
-        /*
-        
-        Rectangle Tribble1.Bounds;
-        Vector2 greySpeed;
-        
-        Rectangle Tribble2.Bounds;
-        Vector2 brownSpeed;
-        
-        Rectangle tribble3.Bounds;
-        Vector2 creamSpeed;
-        
-        Rectangle tribble4.Bounds;
-        Vector2 orangeSpeed;
-        */
         Texture2D brownTexture;
         Texture2D orangeTexture;
         Texture2D creamTexture;
         Texture2D greyTexture;
-        Tribble tribble1;
-        Tribble tribble2;
-        Tribble tribble3;
-        Tribble tribble4;
+        List<Tribble> tribbles = new List<Tribble>();
         SoundEffect cooSound;
         SoundEffectInstance coo;
         SoundEffect music;
@@ -64,25 +48,13 @@ namespace monogame3
             _graphics.PreferredBackBufferHeight = 600;
             _graphics.ApplyChanges();
             this.Window.Title = "they've got the moves";
-            /*
-            Tribble1.Bounds = new Rectangle(300, 10, 100, 100);
-            greySpeed = new Vector2(2, 2);
-            Tribble2.Bounds = new Rectangle(0, 430, 100, 100);
-            brownSpeed = new Vector2(2, 3);
-            tribble3.Bounds = new Rectangle(0, 0, 50, 50);
-            creamSpeed = new Vector2(5, 5);
-            tribble4.Bounds = new Rectangle(350, 250, 100, 100);
-            do
-            {
-                orangeSpeed = new Vector2(gen.Next(-4, 5), gen.Next(-4, 5));
-            } while (orangeSpeed.Equals((0, 0)));
-            */
             screen = Screen.Intro;
             base.Initialize();
-            tribble1 = new Tribble(greyTexture, new Rectangle(300, 10, 100, 100), new Vector2(2, 2));
-            tribble2 = new Tribble(brownTexture, new Rectangle(0, 430, 100, 100), new Vector2(2, 3));
-            tribble3 = new Tribble(creamTexture, new Rectangle(0, 0, 50, 50), new Vector2(5, 5));
-            tribble4 = new Tribble(orangeTexture, new Rectangle(350, 250, 100, 100), new Vector2(3, 4));
+            tribbles.Add(new Tribble(greyTexture, new Rectangle(300, 10, 100, 100), new Vector2(2, 2), 0));
+            tribbles.Add(new Tribble(brownTexture, new Rectangle(0, 430, 100, 100), new Vector2(2, 3), 0));
+            tribbles.Add(new Tribble(creamTexture, new Rectangle(0, 0, 50, 50), new Vector2(5, 5), 1));
+            tribbles.Add(new Tribble(orangeTexture, new Rectangle(350, 250, 100, 100), new Vector2(3, 4), 2));
+            tribbles.Add(new Tribble(brownTexture));
         }
 
         protected override void LoadContent()
@@ -104,7 +76,6 @@ namespace monogame3
             textFont = Content.Load<SpriteFont>("text");
             // TODO: use this.Content to load your game content here
         }
-
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
@@ -123,70 +94,13 @@ namespace monogame3
             else if (screen == Screen.TribbleYard)
             {
                 time = (float)gameTime.TotalGameTime.TotalSeconds - timeStamp;
-
-                tribble1.Move();
-                if (tribble1.Bounds.Right >= _graphics.PreferredBackBufferWidth || tribble1.Bounds.Left <= 0)
+                foreach (Tribble tribble in tribbles)
                 {
-                    tribble1.BounceLeftRight();
-                    coo.Play();
-                }
-                if (tribble1.Bounds.Bottom > _graphics.PreferredBackBufferHeight || tribble1.Bounds.Top <= 0)
-                {
-                    tribble1.BounceTopBottom();
-                    coo.Play();
+                    tribble.Move(_graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
+                    if (tribble.HitWall)
+                        coo.Play();
                 }
 
-                tribble3.Move();
-                if (tribble3.Bounds.Left >= _graphics.PreferredBackBufferWidth)
-                    tribble3.Bounds = new Rectangle(-50, tribble3.Bounds.Y, 50, 50);
-                if (tribble3.Bounds.Top >= _graphics.PreferredBackBufferHeight)
-                    tribble3.Bounds = new Rectangle(tribble3.Bounds.X, -50, 50, 50);
-
-                tribble2.Move();
-                if (tribble2.Bounds.Right >= _graphics.PreferredBackBufferWidth || tribble2.Bounds.Left <= 0)
-                {
-                    tribble2.BounceLeftRight();
-                    coo.Play();
-                }
-                if (tribble2.Bounds.Bottom > _graphics.PreferredBackBufferHeight || tribble2.Bounds.Top <= 0)
-                {
-                    tribble2.BounceTopBottom();
-                    coo.Play();
-                }
-
-                if (tribble4.Bounds.Top > _graphics.PreferredBackBufferHeight || tribble4.Bounds.Bottom < 0 || tribble4.Bounds.Left > _graphics.PreferredBackBufferWidth || tribble4.Bounds.Right < 0)
-                {
-                    int newX;
-                    int newY;
-                    int newSize = gen.Next(70, 131);
-                    int side = gen.Next(4);
-                    switch (side)
-                    {
-                        default:
-                            newX = 0 - tribble4.Bounds.Width;
-                            newY = gen.Next(_graphics.PreferredBackBufferHeight - tribble4.Bounds.Height);
-                            tribble4.Speed = new Vector2(gen.Next(1, 5), gen.Next(-4, 5));
-                            break;
-                        case 1:
-                            newY = 0 - tribble4.Bounds.Height;
-                            newX = gen.Next(_graphics.PreferredBackBufferWidth - tribble4.Bounds.Width);
-                            tribble4.Speed = new Vector2(gen.Next(-4, 5), gen.Next(1, 5));
-                            break;
-                        case 2:
-                            newX = _graphics.PreferredBackBufferWidth;
-                            newY = gen.Next(_graphics.PreferredBackBufferHeight - tribble4.Bounds.Height);
-                            tribble4.Speed = new Vector2(gen.Next(1, 5) * -1, gen.Next(-4, 5));
-                            break;
-                        case 3:
-                            newY = _graphics.PreferredBackBufferHeight;
-                            newX = gen.Next(_graphics.PreferredBackBufferWidth - tribble4.Bounds.Width);
-                            tribble4.Speed = new Vector2(gen.Next(-4, 5), gen.Next(1, 5) * -1);
-                            break;
-                    }
-                    tribble4.Bounds = new Rectangle(newX, newY, newSize, newSize);
-                }
-                tribble4.Move();
-                
                 if (playMusic.State == SoundState.Stopped)
                 {
                     screen = Screen.End;
@@ -210,10 +124,8 @@ namespace monogame3
             else if (screen == Screen.TribbleYard)
             {
                 _spriteBatch.DrawString(textFont, (music.Duration.TotalSeconds - time).ToString("N0"), new Vector2(0, 0), Color.Green);
-                _spriteBatch.Draw(tribble3.Texture, tribble3.Bounds, Color.White);
-                _spriteBatch.Draw(tribble1.Texture, tribble1.Bounds, Color.White);
-                _spriteBatch.Draw(tribble2.Texture, tribble2.Bounds, Color.White);
-                _spriteBatch.Draw(tribble4.Texture, tribble4.Bounds, Color.White);
+                foreach (Tribble tribble in tribbles)
+                    _spriteBatch.Draw(tribble.Texture, tribble.Bounds, Color.White);
             }
             else
             {
--- Tribble.cs
+++ Tribble.cs
@@ -11,6 +11,9 @@ namespace monogame3
         private Texture2D _texture;
         private Rectangle _rectangle;
         private Vector2 _speed;
+        private int _edgeBehaviour;
+        private bool _hitWall;
+        /// 0-bounce 1-loop 2-teleport
         Random gen = new Random();
 
         public Tribble(Texture2D texture, Rectangle rectangle, Vector2 speed)
@@ -18,15 +21,24 @@ namespace monogame3
             _texture = texture;
             _rectangle = rectangle;
             _speed = speed;
+            _edgeBehaviour = gen.Next(3);
+        }
+        public Tribble(Texture2D texture, Rectangle rectangle, Vector2 speed, int behaviour)
+        {
+            _texture = texture;
+            _rectangle = rectangle;
+            _speed = speed;
+            _edgeBehaviour = behaviour;
         }
         public Tribble(Texture2D texture)
         {
             _texture = texture;
-            _rectangle = new Rectangle(gen.Next(800), gen.Next(600), gen.Next(40, 120), gen.Next(40, 120));
+            _rectangle = new Rectangle(new Point(gen.Next(800), gen.Next(600)), new Point(gen.Next(40, 131)));
             do
             {
                 _speed = new Vector2(gen.Next(-4, 5), gen.Next(-4, 5));
             } while (_speed.Equals((0, 0)));
+            _edgeBehaviour = gen.Next(3);
         }
         public Texture2D Texture
         {
@@ -42,17 +54,74 @@ namespace monogame3
             get { return _speed; }
             set { _speed = value; }
         }
-        public void Move()
+
+        public bool HitWall
+        {
+            get { return _hitWall; }
+        }
+
+        public void Move(int width, int height)
         {
             _rectangle.Offset(_speed);
-        }
-        public void BounceLeftRight()
-        {
-            _speed.X *= -1;
-        }
-        public void BounceTopBottom()
-        {
-            _speed.Y *= -1;
+            _hitWall = false;
+            if (_edgeBehaviour == 0)
+            {
+                if (Bounds.Right >= width || Bounds.Left <= 0)
+                {
+                    _speed.X *= -1;
+                    _hitWall = true;
+                }
+                if (Bounds.Bottom > height || Bounds.Top <= 0)
+                {
+                    _speed.Y *= -1;
+                    _hitWall = true;
+                }
+            }
+            else if (_edgeBehaviour == 1)
+            {
+                if (Bounds.Left > width)
+                    Bounds = new Rectangle(new Point(0 - _rectangle.Width, _rectangle.Y), _rectangle.Size);
+                if (Bounds.Right < 0)
+                    Bounds = new Rectangle(new Point(width, _rectangle.Y), _rectangle.Size);
+                if (Bounds.Top > height)
+                    Bounds = new Rectangle(new Point(_rectangle.X, 0 - _rectangle.Height), _rectangle.Size);
+                if (Bounds.Bottom < 0)
+                    Bounds = new Rectangle(new Point(_rectangle.X, height), _rectangle.Size);
+            }
+            else
+            {
+                if (Bounds.Top > height || Bounds.Bottom < 0 || Bounds.Left > width || Bounds.Right < 0)
+                {
+                    int newX;
+                    int newY;
+                    int newSize = gen.Next(70, 131);
+                    int side = gen.Next(4);
+                    switch (side)
+                    {
+                        default:
+                            newX = 0 - Bounds.Width;
+                            newY = gen.Next(height - Bounds.Height);
+                            _speed = new Vector2(gen.Next(1, 5), gen.Next(-4, 5));
+                            break;
+                        case 1:
+                            newY = 0 - Bounds.Height;
+                            newX = gen.Next(width - Bounds.Width);
+                            _speed = new Vector2(gen.Next(-4, 5), gen.Next(1, 5));
+                            break;
+                        case 2:
+                            newX = width;
+                            newY = gen.Next(height - Bounds.Height);
+                            _speed = new Vector2(gen.Next(1, 5) * -1, gen.Next(-4, 5));
+                            break;
+                        case 3:
+                            newY = height;
+                            newX = gen.Next(width - Bounds.Width);
+                            _speed = new Vector2(gen.Next(-4, 5), gen.Next(1, 5) * -1);
+                            break;
+                    }
+                    Bounds = new Rectangle(newX, newY, newSize, newSize);
+                }
+            }
         }
     }
 }
